aae847b91665ed15cfa2f9a67a52f53384385a93
abhitsahota <abhit.sahota@gmail.com>
2020-09-20 15:45:11 -0400

    completed bft, dft and other graph methods

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -13,33 +13,97 @@ class Graph:
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        # Create the new key with the vertex ID and se thte value to an empty set (meaning no edges yet)
+        try:
+            if vertex_id not in self.vertices:
+                self.vertices[vertex_id] = set()
+        except:
+            return Exception
 
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        # Find vertex v1 and add v2 to the set of edges
+        try: 
+            if v1 in self.vertices and v2 in self.vertices:
+                self.vertices[v1].add(v2)
+        except:
+            return Exception
 
     def get_neighbors(self, vertex_id):
         """
         Get all neighbors (edges) of a vertex.
         """
-        pass  # TODO
+        try: 
+            if vertex_id in self.vertices:
+                return self.vertices[vertex_id]
+        except: 
+            return Exception
+
 
     def bft(self, starting_vertex):
         """
         Print each vertex in breadth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        # Create an empty queue and enqueue the starting_vertex
+        # create empty set to track visited verticies
+
+        #while the queue is not empty
+            # get th current vertex (dequeue from queue)
+
+            # Check if the current vertex has been visited
+                # print the current vertes
+                # mark the current vertex as visited
+                    # add the current vertex to a visited set
+
+                # queue up all the current vertex's neighhours (seo we can visit them next)
+
+        the_que = Queue()
+        the_que.enqueue(starting_vertex)
+        track_set = set()
+
+        while the_que.size() > 0:
+            curr = the_que.dequeue()
+        
+            if curr not in track_set:
+                print(curr)
+                track_set.add(curr)
+                for neighbour in self.vertices[curr]:
+                    the_que.enqueue(neighbour)
+
 
     def dft(self, starting_vertex):
         """
         Print each vertex in depth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        # Create an empty stack and add the starting_vertex
+        # create empty set to track visited verticies
+
+        #while the stack is not empty
+            # get th current vertex (dequeue from queue)
+
+            # Check if the current vertex has been visited
+                # print the current vertes
+                # mark the current vertex as visited
+                    # add the current vertex to a visited set
+
+                # push up all the current vertex's neighhours (seo we can visit them next)
+        the_stack = Stack()
+        the_stack.push(starting_vertex)
+        track_set = set()
+
+        while the_stack.size() > 0:
+            curr = the_stack.pop()
+        
+            if curr not in track_set:
+                print(curr)
+                track_set.add(curr)
+                for neighbour in self.vertices[curr]:
+                    the_stack.push(neighbour)
+
 
     def dft_recursive(self, starting_vertex):
         """
@@ -56,6 +120,31 @@ class Graph:
         starting_vertex to destination_vertex in
         breath-first order.
         """
+        # We want to track if there is a path between the verticies and what the shortest path is
+
+        # Create an empty queue and enqueue the path to the (attaching a list within the queue) starting_vertex
+        # create empty set to track visited verticies
+
+
+
+        #while the queue is not empty
+            # get th current vertex path (dequeue from queue)
+            # set the current vertes t the last element of the path
+
+            # Check if the current vertex has been visited
+
+                # Check if the current verties is the destination
+                    # if it is, then return
+
+                # mark the current vertex as visited
+                    # add the current vertex to a visited set
+                    
+                # queue up news path with each path
+                    # take current path
+                    # append the neight to it
+                    # queue up the new path
+
+
         pass  # TODO
 
     def dfs(self, starting_vertex, destination_vertex):
